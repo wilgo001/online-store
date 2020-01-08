df8b2dda986af231bd948b1f136628166d83f1fe
Corentin <i181624>
2020-01-08 17:36:51 +0100

    doucement, daccord !

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,13 +4,21 @@ echo'
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>TP1 - Exercice 1</title>
+	<title>SITE</title>
 </head>
 <body>
 <form method="post" action="../gestion_list/ajout">
 	Login : <input type="text" name="login"><br>
 	Pass : <input type="text" name="password"><br>
 	<input type="submit" value="Se connecter">
+</form>
+
+
+<form method="post" action="../../index.php/connection/inscription">
+    Login : <input type="text" name="login"><br/>
+    Password : <input type="password" name="password1"><br/>
+    Confirm Password : <input type="password" name="password2"><br/>
+    <input type="submit" value="S\'inscrire">
 </form>
 
 </body>
